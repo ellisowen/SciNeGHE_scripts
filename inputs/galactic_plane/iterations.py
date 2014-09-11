ec5fceda0a5cf238efce26413e38e02042386a00
Ellis Owen <ellis.owen@mpi-hd.mpg.de>
2014-09-11 15:07:18 +0200

    some changes to iterations.py script

--- inputs/galactic_plane/iterations.py
+++ inputs/galactic_plane/iterations.py
@@ -23,20 +23,22 @@ images = GammaImages(counts=counts, background=background)
 source_kernel = binary_disk(3).astype(float)
 source_kernel /= source_kernel.sum()
 
-background_kernel = np.ones((10, 100))
+background_kernel = np.ones((5, 50))
+background_kernel /= background_kernel.sum()
 
 # *** ITERATOR ***
 
 ibe = IterativeKernelBackgroundEstimator(images=images,
                                          source_kernel=source_kernel,
                                          background_kernel=background_kernel,
-                                         significance_threshold=0.7,
+                                         significance_threshold=4,
                                          mask_dilation_radius=3,
                                          save_intermediate_results=True
                                          )
 
-n_iterations = 3
-
+ibe.run(filebase='TestOutput')
+#n_iterations = 3
+"""
 # *** RUN & PLOT ***
 plt.figure(figsize=(4,6))
 plt.rc('text', usetex=True)
@@ -62,12 +64,15 @@ for iteration in range(1, n_iterations):
 
     mask_hdu = ibe.mask_image_hdu
     mask = mask_hdu.data[:, 700:1400]
+    mask_hdu.writeto('mask_{0}.fits'.format(iteration))
+    
+   # import IPython; IPython.embed()
 
     plt.subplot(n_iterations+3, 1, 2*iteration+1)
     significance_hdu = ibe.significance_image_hdu
     data = significance_hdu.data[:, 700:1400]
     plt.imshow(data, vmin=-3, vmax=5)
-    #plt.contour(mask, levels=[0], linewidths=1, colors='black')
+    plt.contour(mask, levels=[0], linewidths=1, colors='black')
     plt.axis('off')
     plt.title(r'Significance Image, Iteration {0}'.format(iteration), fontsize='small')
 
@@ -77,9 +82,10 @@ for iteration in range(1, n_iterations):
     background_hdu = ibe.background_image_hdu
     data = background_hdu.data[:, 700:1400]
     plt.imshow(data)#, vmin=0, vmax=0.2)
-    #plt.contour(mask, levels=[0], linewidths=1, colors='white')
+    plt.contour(mask, levels=[0], linewidths=1, colors='white')
     plt.axis('off')
     plt.title(r'Background Estimation, Iteration {0}'.format(iteration), fontsize='small')
 
 plt.tight_layout()
 plt.savefig('iterations.pdf')
+"""
